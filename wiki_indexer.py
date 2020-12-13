01ea095f247d1447eb8ad54033a9c0f76c1c9f34
Coder-12 <31615917+Coder-12@users.noreply.github.com>
2020-12-13 17:32:15 +0530

    Add files via upload

---
+++ wiki_indexer.py
@@ -0,0 +1,76 @@
+from xml.sax import ContentHandler,parse
+import sys
+import re
+import time
+from text_processing import *
+from nltk.stem import PorterStemmer
+from collections import defaultdict
+
+Title_File="Title.txt"
+Title_File_Name=open(Title_File,'a')
+
+# Define Sax parser Class
+class Parser_Module(ContentHandler):
+     def __init__(self):
+         self.dno=0 
+         self.Catagory="" 
+         self.title=""
+         self.Pagetitle=""
+         self.text=""
+         self.id=""
+         self.f=0
+
+     def startElement(self,tag,attributes):
+         if(tag=="title"):
+            self.Catagory="title"
+            self.title=""
+            self.f=1
+         if(tag=="page"):
+            self.dno+=1
+            #self.Catagory="page"
+         if(tag=="text"):
+            self.Catagory="text"
+            self.text=""
+         if(tag=="id" and self.f):
+            self.Catagory="id"
+            self.id=""
+
+     def endElement(self,tag):
+         if(tag=="title"):
+            Parse_Title(self.title,self.dno)
+            self.Pagetitle=self.title
+            self.title=""
+            self.Catagory=""
+         elif(tag=="text"):
+            Parse_Text(self.text,self.dno)
+            self.text=""
+            self.Catagory=""
+         elif(tag=="id" and self.f):
+            try:
+                Title_File_Name.write(str(self.dno)+"#"+self.Pagetitle+":"+self.id+"\n")
+            except:
+                Title_File_Name.write(str(self.dno)+"#"+self.Pagetitle.encode('utf-8')+":"+self.id.encode('utf-8')+"\n")
+            self.f=0
+            self.id=""
+            self.Catagory=""
+
+     def characters(self,content):
+        if(self.Catagory=="text"):
+           self.text+=content
+        elif(self.Catagory=="title"):
+           self.title+=content
+        elif(self.Catagory=="id"):
+           self.id+=content
+
+print("processing...")
+start=time.time()
+parse_handle=Parser_Module()
+for i in range(1,35,1):
+     parse("wiki"+str(i)+".xml",parse_handle)
+     #print("Parsing completed and Store parse info....\n")
+     write_index_File("./Data/index_"+str(var)+".txt")
+end=time.time()
+print("Time taken for parsing: ",end-start)
+
+#write_stat_File("./Data/index_stat.txt")
+print("Everything completed")
